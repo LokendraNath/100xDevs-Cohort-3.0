8ec2bca9335f68421d761cac9973c7fc42404364
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-12 20:59:17 +0530

    index signature

--- WebDev/14.3-TypeScript-Adw-API/src/index.ts
+++ WebDev/14.3-TypeScript-Adw-API/src/index.ts
@@ -1,1 +1,19 @@
+type User = {
+  id: number;
+  name: string;
+};
 
+type Users = {
+  [key: string]: User;
+};
+
+const users: Users = {
+  "loki@gdfk": {
+    id: 23,
+    name: "Loki Thanos",
+  },
+  "Shabi@gaidf": {
+    id: 33,
+    name: "Shabib Shaik",
+  },
+};
