ebf2ae345b99d890a6b49046ea17009483d140b9
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-12 20:09:31 +0530

    clear

--- WebDev/14.3-TypeScript-Adw-API/src/index.ts
+++ WebDev/14.3-TypeScript-Adw-API/src/index.ts
@@ -1,14 +1,1 @@
-interface User {
-  id: number;
-  name: string;
-  age: number;
-  email: string;
-  password: string;
-}
 
-type UpdateProps = Pick<User, "name" | "age" | "email">;
-type UpdatePropsOptional = Partial<User>;
-
-function updateUser(updatedProps: UpdatePropsOptional) {
-  // hit the database tp update the user
-}
